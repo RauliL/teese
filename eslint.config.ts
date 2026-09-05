06b97b078c898325b21e8a8bc6edb818224ed4e6
Rauli Laine <rauli.laine@treet.fi>
2026-09-05 20:35:43 +0300

    Add FormatJS ESLint plugin

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,5 @@
 import js from "@eslint/js";
+import formatjs from "eslint-plugin-formatjs";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
@@ -7,7 +8,7 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    plugins: { js },
+    plugins: { js, formatjs },
     extends: ["js/recommended"],
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
